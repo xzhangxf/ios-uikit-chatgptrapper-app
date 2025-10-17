1bd7913edab2ebe7597ee03a462a14f34bc4bd58
xzhangxf <565772944@qq.com>
2025-10-17 13:48:50 +0800

    rename ViewController to ChatViewController

--- ChatGPTRapper/ChatViewController.swift
+++ ChatGPTRapper/ChatViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChatViewController.swift
 //  ChatGPTRapper
 //
 //  Created by Xufeng Zhang on 17/10/25.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- ChatGPTRapper/SceneDelegate.swift
+++ ChatGPTRapper/SceneDelegate.swift
@@ -25,7 +25,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         //        window?.makeKeyAndVisible()
                 guard let windowScene = (scene as? UIWindowScene) else { return }
                 window = UIWindow(windowScene: windowScene)
-                window?.rootViewController = ViewController() //window will show on the viewcontroller
+                window?.rootViewController = ChatViewController() //window will show on the viewcontroller
                 window?.makeKeyAndVisible()
     }
 
